bab0ea892373ed138da9d3c87466b0e8dd1c9c3e
KasparSiivelt5 <89830554+KasparSiivelt5@users.noreply.github.com>
2021-09-07 12:19:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PositiveNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm palub sisestada numbrit
+            //programm kontrollib numbrit
+            //kui number on positiivne ,konsool kuvab "number on positiivne";
+            //kui number on negatiivne ,konsool kuvab "number on negatiivne";
+            //kui sisestatud number on 0 ,siis konsool kuvab "null";
+            //NB! int32.Parse asemel kasutame Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Palun sisesta number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            if (Number > 0)
+            {
+                Console.WriteLine("Number on negatiivne");
+
+             {else if (Number < 0);
+
+                    Console.WriteLine("Number on positiivne");
+
+
+                      
+                     
+                
+             }
+         
+        }
+    }
+}
